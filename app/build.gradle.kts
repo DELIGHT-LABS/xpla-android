b4da2ae5e428b8d4c71b707542395bbc98b56799
tlguszz10 <tlguszz10@gmail.com>
2024-03-21 10:42:35 +0900

    feat: add protofiles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
+    id("com.google.protobuf") version "0.9.4"
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.9.4")
+    }
 }
 
 android {
@@ -32,8 +39,14 @@ android {
     }
 }
 
-dependencies {
+val grpcVersion = "1.54.0"
+val grpcKotlinVersion = "1.4.1"
+val protobufVersion = "4.26.0"
 
+dependencies {
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -41,4 +54,56 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc") {
+                    option("lite")
+                }
+                create("grpckt") {
+                    option("lite")
+                }
+            }
+            it.builtins {
+                create("java") {
+                    option("lite")
+                }
+                create("kotlin") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
+sourceSets {
+    arrayOf("debug", "release", "main").forEach { sourceSetName ->
+        val sourceSet = findByName(sourceSetName)
+        if (sourceSet != null) {
+            sourceSet.java.srcDirs(
+                "build/generated/source/proto/main/java",
+                "build/generated/source/proto/main/kotlin",
+            )
+            ((sourceSet as ExtensionAware).extensions["proto"] as SourceDirectorySet).srcDir(
+                "src/main/proto"
+            )
+        }
+    }
+
 }
