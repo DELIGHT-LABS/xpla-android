1b513512f79f71b84091edefadc38c8774fe3bda
Minji Kim <minji@delightlabs.io>
2024-06-05 02:30:10 +0900

    feat: wallet with privateKey only

--- app/src/main/java/io/delightlabs/xplaandroid/LCDClient.kt
+++ app/src/main/java/io/delightlabs/xplaandroid/LCDClient.kt
@@ -8,6 +8,7 @@ import io.delightlabs.xplaandroid.api.BankAPI
 import io.delightlabs.xplaandroid.api.TxAPI
 import io.delightlabs.xplaandroid.api.WasmAPI
 import io.delightlabs.xplaandroid.api.XplaNetwork
+import wallet.core.jni.PrivateKey
 
 const val PubkeyProtoType: String = "/ethermint.crypto.v1.ethsecp256k1.PubKey"
 
@@ -29,6 +30,11 @@ class LCDClient(
     fun wallet(mnemonic: String, passphrase: String = ""): LCDWallet {
         System.loadLibrary("TrustWalletCore")
         return LCDWallet(this, mnemonic, passphrase)
+    }
+
+    fun wallet(privateKey: PrivateKey): LCDWallet {
+        System.loadLibrary("TrustWalletCore")
+        return LCDWallet(this, privateKey)
     }
 }
 
