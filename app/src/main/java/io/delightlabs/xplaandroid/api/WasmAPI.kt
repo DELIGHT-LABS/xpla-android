4230cd1a63a450971ba45e595880f762207232cc
Minji Kim <minji@delightlabs.io>
2024-06-05 17:47:00 +0900

    feat: contractQuery - encodedString trim

--- app/src/main/java/io/delightlabs/xplaandroid/api/WasmAPI.kt
+++ app/src/main/java/io/delightlabs/xplaandroid/api/WasmAPI.kt
@@ -1,6 +1,9 @@
 package io.delightlabs.xplaandroid.api
 
 import android.util.Base64
+import android.util.Log
+import androidx.core.net.toUri
+import java.net.URLEncoder
 
 class WasmAPI(val apiRequester: APIRequester) {
 
@@ -8,10 +11,11 @@ class WasmAPI(val apiRequester: APIRequester) {
         contractAddr: String,
         queryJson: String
     ):T? {
-        val encodedString: String = Base64.encodeToString(queryJson.toByteArray(), 0)
+        val encodedString: String = Base64.encodeToString(queryJson.toByteArray(), 0).trim().replace("\n","")
+        val path = Endpoint.SmartQuery(contractAddr, encodedString).path
         return apiRequester.request<T>(
             HttpMethod.GET,
-            Endpoint.SmartQuery(contractAddr, encodedString.trim()).path)
+            path)
     }
 
 
