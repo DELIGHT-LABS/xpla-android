8e331e08a6b70cd87a5e4fcb3ce14298b668236c
JoowonYun <joowon@delightlabs.io>
2024-06-10 21:57:18 +0900

    fix: temporarily corresponds to a different sign

--- app/src/main/java/io/delightlabs/xplaandroid/core/Fee.kt
+++ app/src/main/java/io/delightlabs/xplaandroid/core/Fee.kt
@@ -1,25 +1,35 @@
 package io.delightlabs.xplaandroid.core
 
 
+import com.google.gson.annotations.Expose
 import cosmos.tx.v1beta1.TxOuterClass.Fee
 import java.io.Serializable
 
+val chainCosmos47: Array<String> = arrayOf("cube_47-5")
+fun isCosmos47Chain(chainID: String?): Boolean {
+    return chainID?.let {
+        it.isNotEmpty() && chainCosmos47.contains(it)
+    } ?: false
+}
 
 data class StdFee(
     var amount: List<Coin> = listOf(),
     var gas: String = "",
     var payer: String? = null,
-    var granter: String? = null
+    var granter: String? = null,
+    @Expose
+    var chainID: String? = null
+
 ) : Serializable {
-    constructor(fee: Fee) : this() {
+    constructor(fee: Fee, chainID: String?) : this() {
         this.amount = fee.amountList.map {
             Coin(amount = it.amount, denom = it.denom)
         }
         this.gas = fee.gasLimit.toString()
-        if(!fee.payer.isNullOrBlank()) {
+        if(isCosmos47Chain(chainID) && !fee.payer.isNullOrBlank()) {
             this.payer = fee.payer
         }
-        if(!granter.isNullOrBlank()) {
+        if(isCosmos47Chain(chainID) && !granter.isNullOrBlank()) {
             this.granter = fee.granter
         }
     }
