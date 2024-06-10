457c3605b56c1107ec22965a960cd337cc9552a8
JoowonYun <joowon@delightlabs.io>
2024-06-10 14:53:23 +0900

    feat: amino for cube

--- app/src/main/java/io/delightlabs/xplaandroid/core/Fee.kt
+++ app/src/main/java/io/delightlabs/xplaandroid/core/Fee.kt
@@ -1,17 +1,26 @@
 package io.delightlabs.xplaandroid.core
 
-import com.google.gson.annotations.SerializedName
+
 import cosmos.tx.v1beta1.TxOuterClass.Fee
 import java.io.Serializable
 
+
 data class StdFee(
     var amount: List<Coin> = listOf(),
-    var gas: String = ""
+    var gas: String = "",
+    var payer: String? = null,
+    var granter: String? = null
 ) : Serializable {
     constructor(fee: Fee) : this() {
         this.amount = fee.amountList.map {
             Coin(amount = it.amount, denom = it.denom)
         }
         this.gas = fee.gasLimit.toString()
+        if(!fee.payer.isNullOrBlank()) {
+            this.payer = fee.payer
+        }
+        if(!granter.isNullOrBlank()) {
+            this.granter = fee.granter
+        }
     }
 }
