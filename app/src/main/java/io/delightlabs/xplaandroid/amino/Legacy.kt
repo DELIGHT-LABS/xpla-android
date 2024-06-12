fa46107994e4a98aee87dd28667bd12bc13656c2
JoowonYun <joowon@delightlabs.io>
2024-06-12 15:11:13 +0900

    feat: support to amino (only cw20 transfer in MsgExecuteContract)

--- app/src/main/java/io/delightlabs/xplaandroid/amino/Legacy.kt
+++ app/src/main/java/io/delightlabs/xplaandroid/amino/Legacy.kt
@@ -2,9 +2,9 @@ package io.delightlabs.xplaandroid.amino
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import com.google.protobuf.Any
-import cosmos.bank.v1beta1.Tx
 import java.io.Serializable
 import io.delightlabs.xplaandroid.core.bank.v1beta1.MsgSend
+import io.delightlabs.xplaandroid.core.wasm.v1.MsgExecuteContract
 
 data class AminoType(
     @SerializedName("type")
@@ -29,8 +29,13 @@ data class AminoMsg(
     override fun toAmino(): AminoType {
         when (protoMsg.typeUrl) {
             "/cosmos.bank.v1beta1.MsgSend" -> {
-                val msg = Tx.MsgSend.parseFrom(protoMsg.value.toByteArray())
+                val msg = cosmos.bank.v1beta1.Tx.MsgSend.parseFrom(protoMsg.value.toByteArray())
                 val aminoMsg = MsgSend(msg)
+                return aminoMsg.toAmino()
+            }
+            "/cosmwasm.wasm.v1.MsgExecuteContract" -> {
+                val msg = cosmwasm.wasm.v1.Tx.MsgExecuteContract.parseFrom(protoMsg.value.toByteArray())
+                val aminoMsg = MsgExecuteContract(msg)
                 return aminoMsg.toAmino()
             }
             else -> {
