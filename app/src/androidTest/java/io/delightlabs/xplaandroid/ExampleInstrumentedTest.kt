ff7bf241a3a50902b28ef904dfdfc5ffb45fb39b
Minji Kim <minji@delightlabs.io>
2024-06-17 22:43:27 +0900

    misc: swap test code

--- app/src/androidTest/java/io/delightlabs/xplaandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/delightlabs/xplaandroid/ExampleInstrumentedTest.kt
@@ -1,10 +1,13 @@
 package io.delightlabs.xplaandroid
 
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.gson.Gson
 import com.google.protobuf.Any
+import com.google.protobuf.kotlin.toByteStringUtf8
 import cosmos.bank.v1beta1.msgSend
 import cosmos.base.v1beta1.CoinOuterClass.Coin
+import cosmwasm.wasm.v1.msgExecuteContract
 import io.delightlabs.xplaandroid.api.APIRequester
 import io.delightlabs.xplaandroid.api.APIReturn
 import io.delightlabs.xplaandroid.api.HttpMethod
@@ -104,6 +107,99 @@ class ExampleInstrumentedTest {
                 .build()
 
             val createTx = it.createAndSignTx(
+                CreateTxOptions(msgs = listOf(any))
+            )
+
+            val broadcastRes = txAPI.broadcast(createTx)
+            println("broadcastRes: $broadcastRes")
+        }
+    }
+
+
+    @Test
+    fun testSwapCW20 () {
+
+        val seedPhrase =
+            "table dinner sibling crisp hen genuine wing volume sport north omit cushion struggle script dinosaur merge medal visa also mixture faint surge boy wild"
+        lcd.wallet(mnemonic = seedPhrase).let { wallet ->
+            println(wallet.address)
+            val offerAmount = "1000000" // 1 ctxt
+            // cw20 -> anything
+            val contractAddr = "xpla123dl27tlhs4lyvywarnrydtdr8kca2fy2r75ck3qzpupy5dsdwvqxu5vy0"
+            val send = mapOf(
+                "send" to mapOf(
+                    "contract" to contractAddr,
+                    "amount" to offerAmount,
+                    "msg" to "eyJzd2FwIjp7fX0="
+                )
+            )
+
+            Log.d("send", "$send")
+            val jsonString = Gson().toJson(send)
+            Log.d("transferJsonString", jsonString)
+            val msgExecute = msgExecuteContract {
+                this.sender = wallet.address // 보내는 주소
+                this.contract = "xpla19w8vmg7tmh07ztr3v7lq8sdny6jjkj6pk03a7fk52gpgepfxnlgq8g7r50" // token address (from) : ctxt
+                this.msg = jsonString.toByteStringUtf8()
+                this.funds
+            }
+
+            val any = Any.newBuilder()
+                .setTypeUrl("/cosmwasm.wasm.v1.MsgExecuteContract")
+                .setValue(msgExecute.toByteString())
+                .build()
+
+            val createTx = wallet.createAndSignTx(
+                CreateTxOptions(msgs = listOf(any))
+            )
+
+            val broadcastRes = txAPI.broadcast(createTx)
+            println("broadcastRes: $broadcastRes")
+        }
+    }
+
+    @Test
+    fun testSwapNativeToken () {
+
+        val seedPhrase =
+            "table dinner sibling crisp hen genuine wing volume sport north omit cushion struggle script dinosaur merge medal visa also mixture faint surge boy wild"
+        lcd.wallet(mnemonic = seedPhrase).let { wallet ->
+            println(wallet.address)
+            val offerAmount = "10000000000000000000" // 10xpla
+            val denom = "axpla"
+            // native token -> anything
+            val sendCoin = Coin.newBuilder()
+                .setAmount(offerAmount)
+                .setDenom(denom)
+                .build()
+
+            val swap = mapOf(
+                "offer_asset" to mapOf(
+                    "amount" to offerAmount,
+                    "info" to mapOf(
+                        "native_token" to mapOf(
+                            "denom" to denom
+                        )
+                    )
+                )
+            )
+            Log.d("swap", "$swap")
+            val jsonString = Gson().toJson(swap)
+            Log.d("transferJsonString", jsonString)
+            val msgExecute = msgExecuteContract {
+                this.sender = wallet.address // 보내는 주소
+                this.contract = "xpla123dl27tlhs4lyvywarnrydtdr8kca2fy2r75ck3qzpupy5dsdwvqxu5vy0"
+                this.msg = jsonString.toByteStringUtf8()
+                this.funds.add(sendCoin)
+            }
+
+
+            val any = Any.newBuilder()
+                .setTypeUrl("/cosmwasm.wasm.v1.MsgExecuteContract")
+                .setValue(msgExecute.toByteString())
+                .build()
+
+            val createTx = wallet.createAndSignTx(
                 CreateTxOptions(msgs = listOf(any))
             )
 
